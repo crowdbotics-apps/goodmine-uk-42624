e7abdbc2da3d2bd72448854817f39fce750d8fac
Crowdbotics <team@crowdbotics.com>
2023-08-03 19:53:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "goodmine-uk-42624",
+  url: "https://goodmine-uk-42624.botics.co",
+  api: "https://goodmine-uk-42624.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
